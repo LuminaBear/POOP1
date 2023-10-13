12d10214fb351d0bb93c898045e538a7e880be52
LuminaBear <147792595+LuminaBear@users.noreply.github.com>
2023-10-13 00:53:28 -0600

    Creacion de practica

---
+++ src/poop1/POOP1.java
@@ -0,0 +1,20 @@
+/**
+ * Clase principal que contiene el método main.
+ * 
+ * <p>Este programa simple imprime un saludo utilizando el sistema de salida estándar.</p>
+ * 
+ * @author alumno
+ */
+public class POOP1 {
+
+    /**
+     * Punto de entrada principal para la aplicación.
+     * 
+     * <p>Imprime un mensaje de saludo en la consola.</p>
+     * 
+     * @param args Los argumentos de la línea de comandos (no se utilizan en este caso).
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola mundo desde POO grupo 8 desde maven");
+    }
+}
